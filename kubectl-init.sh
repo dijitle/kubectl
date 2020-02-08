43d7292d9828f93adf0a080c656c21aef731612b
Arunas Slepikas <dijitle@gmail.com>
2020-02-08 15:43:29 -0600

    set #!

--- kubectl-init.sh
+++ kubectl-init.sh
@@ -1,4 +1,5 @@
-kubectl config set-credentials default --token${KUBERNETES_TOKEN}
+#!/bin/sh
+kubectl config set-credentials default --token=${KUBERNETES_TOKEN}
 
 echo ${KUBERNETES_CERT} | base64 -d > ca.crt
 kubectl config set-cluster default --server=https://${KUBERNETES_SERVICE_HOST}:${KUBERNETES_SERVICE_PORT_HTTPS} --certificate-authority=ca.crt
